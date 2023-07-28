f8f4770b819870b7dd69a79ff24b95d21720534c
草方块 <grassblock@noreply.codeberg.org>
2023-07-28 13:55:39 +0000

    fix: fcitx5-cht not found in log and can't change its theme

--- scripts/dark.sh
+++ scripts/dark.sh
@@ -3,5 +3,5 @@ notify-send "有月影 在水面 漂流不定"
 cp ~/.config/gtk-4.0/gtk-dark.css ~/.config/gtk-4.0/gtk.css
 kvantummanager --set Nordic-bluish-solid
 notify-send "已设置Qt主题，如果存在部分生效的情况，请注销后重新登录。"
-sh fcitx5-cht.sh catppuccin-mocha
+sh ~/ws/bin/fcitx5-cht.sh catppuccin-mocha
 notify-send "已设置 Fcitx5主题，如果没有生效请重启Fcitx5服务。"
--- scripts/light.sh
+++ scripts/light.sh
@@ -3,5 +3,5 @@ notify-send "漱正阳而含朝霞"
 cp ~/.config/gtk-4.0/gtk-light.css ~/.config/gtk-4.0/gtk.css
 kvantummanager --set VimixDoder
 notify-send "已设置Qt主题，如果存在部分生效的情况，请注销后重新登录。"
-sh fcitx5-cht.sh catppuccin-latte
+sh ~/ws/bin/fcitx5-cht.sh catppuccin-latte
 notify-send "已设置 Fcitx5主题，如果没有生效请重启Fcitx5服务。"
